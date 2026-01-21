8f95abe7c37bef60c717a168a50b423c68456369
Emery <mremmanueledem@gmail.com>
2026-01-21 03:56:55 +0100

    pre: test to see that server is working

---
+++ main.go
@@ -0,0 +1,17 @@
+package main
+
+import (
+	"io"
+	"log"
+	"net/http"
+)
+
+func main() {
+	http.HandleFunc("/", func(w http.ResponseWriter, req *http.Request) {
+		io.WriteString(w, "Hello, Welcome to AiredSpace!\n")
+	})
+
+	log.Printf("Server is running on port :8080. Go to http://localhost:8080")
+	err := http.ListenAndServe(":8080", nil)
+	log.Fatal(err)
+}
