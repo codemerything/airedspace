26adf75eda03abd22b8fad57bb33ee3730e1bfe7
Emery <mremmanueledem@gmail.com>
2026-02-27 14:41:48 +0100

    added more fields (created_at and updated_at) to models.go. the structs here fully represent the data in the database

--- models.go
+++ models.go
@@ -1,10 +1,14 @@
 package main
 
+import "time"
+
 type User struct {
-	UserID   int    `json:"id" db:"id"`
-	Username string `json:"username" db:"username"`
-	Name     string `json:"name" db:"name"`
-	LastName string `json:"lastname" db:"lastname"`
-	Email    string `json:"email" db:"email"`
-	Password string `json:"password" db:"password"`
+	UserID    int64     `json:"id" db:"id"`
+	Username  string    `json:"username" db:"username"`
+	Name      string    `json:"name" db:"name"`
+	LastName  string    `json:"lastname" db:"lastname"`
+	Email     string    `json:"email" db:"email"`
+	Password  string    `json:"password" db:"password"`
+	CreatedAt time.Time `json:"created_at" db:"created_at"`
+	UpdatedAt time.Time `json:"updated_at" db:"updated_at"`
 }
