64af96bb52302445e7f7f6e6a8fc79d3710e4068
Emery <mremmanueledem@gmail.com>
2026-03-01 23:43:09 +0100

    added description of what these files do and also i added logic for what happens in the CreateUser function for service

--- service.go
+++ service.go
@@ -1,4 +1,13 @@
+// the chef: handles business logic
+
 package main
+
+import (
+	"context"
+	"errors"
+
+	"golang.org/x/crypto/bcrypt"
+)
 
 type CreateUserInput struct {
 	Username string
@@ -10,6 +19,32 @@ type Service struct {
 	repo *Repository
 }
 
-func (s *Service) CreateUser(input CreateUserInput) (int64, error) {
+func (s *Service) CreateUser(input CreateUserInput) error {
+	if input.Username == "" {
+		return errors.New("username is required")
+	}
+
+	if input.Email == "" {
+		return errors.New("email is required")
+	}
+
+	if input.Password == "" {
+		return errors.New("password is required")
+	}
+
+	hashedPassword, err := bcrypt.GenerateFromPassword([]byte(input.Password), bcrypt.DefaultCost)
+
+	if err != nil {
+		return errors.New("Hashing failed")
+	}
+
+	user := &User{
+		Username: input.Username,
+		Email:    input.Email,
+		Password: string(hashedPassword),
+	}
+
+	err = s.repo.CreateUser(context.TODO(), user)
+	return err
 
 }
