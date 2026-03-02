b19f6232e818c6b94cd6f274103f11e5ac2f59a8
Emery <mremmanueledem@gmail.com>
2026-03-02 03:31:34 +0100

    added handler for signup which would serve as the background for everything else from sign in to other stuff

---
+++ handler.go
@@ -0,0 +1,47 @@
+package main
+
+import (
+	"encoding/json"
+	"net/http"
+)
+
+type Handler struct {
+	service Service
+}
+
+type SignUpRequest struct {
+	Username string `json:"username"`
+	Email    string `json:"email"`
+	Password string `json:"password"`
+}
+
+func Welcome(w http.ResponseWriter, r *http.Request) {
+	w.Write([]byte("Hello world"))
+}
+
+func SignIn(w http.ResponseWriter, r *http.Request) {
+
+}
+
+func (h *Handler) SignUp(w http.ResponseWriter, r *http.Request) {
+	var user SignUpRequest
+	err := json.NewDecoder(r.Body).Decode(&user)
+
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusBadRequest)
+		return
+	}
+
+	err = h.service.CreateUser(CreateUserInput(user))
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusBadRequest)
+		return
+	}
+
+	w.Header().Set("Content-Type", "application/json")
+	w.WriteHeader(http.StatusOK)
+	json.NewEncoder(w).Encode(map[string]string{"message": "signup successful"})
+
+}
+
+func Movies(w http.ResponseWriter, r *http.Request) {}
