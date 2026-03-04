358ef33b2fc82580cbbb33374d9cf306397edcb1
Emery <mremmanueledem@gmail.com>
2026-03-04 23:17:13 +0100

    add: called the other files in main to be able to use them here

--- main.go
+++ main.go
@@ -1,23 +1,72 @@
 package main
 
 import (
-	"database/sql"
+	"context"
 	"fmt"
-	"io"
 	"log"
 	"net/http"
+	"os"
+	"os/signal"
+	"time"
 
 	_ "github.com/go-sql-driver/mysql"
+	"github.com/joho/godotenv"
 )
 
+type Handlers struct {
+	h *Handler
+}
 
-func main()
+func main() {
 
-	http.HandleFunc("/", func(w http.ResponseWriter, req *http.Request) {
-		io.WriteString(w, "Hello, Welcome to AiredSpace!\n")
-	})
+	if err := godotenv.Load(); err != nil {
+		log.Println("No .env file found")
+	}
 
-	log.Printf("Server is running on port :8080. Go to http://localhost:8080")
-	err := http.ListenAndServe(":8080", nil)
-	log.Fatal(err)
+	cfg, err := Load()
+	if err != nil {
+		log.Fatalf("Failed to load config %v", err)
+	}
+
+	// database connection
+	db, err := NewConnection(cfg.DatabaseURL)
+
+	// setting up routes
+	repo := &Repository{db: db.DB}
+	service := &Service{repo: repo}
+	h := &Handler{service: service}
+
+	mux := http.NewServeMux()
+	mux.HandleFunc("GET /", Welcome)
+	mux.HandleFunc("POST /signin", SignIn)
+	mux.HandleFunc("POST /signup", h.SignUp)
+	mux.HandleFunc("GET /movies", Movies)
+
+	srv := &http.Server{
+		Addr:         fmt.Sprintf(":%d", cfg.Port),
+		ReadTimeout:  cfg.ReadTimeout,
+		WriteTimeout: cfg.WriteTimeout,
+		Handler:      mux,
+	}
+
+	go func() {
+		log.Printf("Starting server on port %d", cfg.Port)
+		if err := srv.ListenAndServe(); err != nil && err != http.ErrServerClosed {
+			log.Fatalf("Server failed: %v", err)
+		}
+	}()
+
+	quit := make(chan os.Signal, 1)
+	signal.Notify(quit, os.Interrupt)
+	<-quit
+
+	log.Println("Shutting off server...")
+	ctx, cancel := context.WithTimeout(context.Background(), 5*time.Second)
+	defer cancel()
+
+	if err := srv.Shutdown(ctx); err != nil {
+		log.Fatalf("Server forced to shutdown: %v", err)
+	}
+
+	log.Println("Server exited")
 }
