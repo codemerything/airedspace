2c7dbd7bbb031342c40fe034b19f8a4a349df039
Emery <mremmanueledem@gmail.com>
2026-03-09 11:55:59 +0100

    feat: added jwt token generator plus the signin service

--- service.go
+++ service.go
@@ -7,6 +7,7 @@ import (
 	"errors"
 	"time"
 
+	"github.com/golang-jwt/jwt/v5"
 	"golang.org/x/crypto/bcrypt"
 )
 
@@ -19,10 +20,42 @@ type CreateUserInput struct {
 	CreatedAt time.Time
 	UpdatedAt time.Time
 }
+type LoginInput struct {
+	Username string
+	Password string
+}
 
 type Service struct {
 	repo *Repository
 }
+
+type Claims struct {
+	Username string `json:"username"`
+}
+
+// how to generate a jwt token
+
+var mySecretKey = []byte("secretsecret")
+
+func generateToken(username string) (string, error) {
+	claims := jwt.MapClaims{
+		"username": username,
+		"exp":      time.Now().Add(time.Hour * 24).Unix(),
+		"iat":      time.Now().Unix(),
+	}
+
+	token := jwt.NewWithClaims(jwt.SigningMethodHS256, claims)
+
+	tokenString, err := token.SignedString(mySecretKey)
+
+	if err != nil {
+		return "", err
+	}
+
+	return tokenString, nil
+}
+
+// jwt token ends
 
 func (s *Service) CreateUser(input CreateUserInput) error {
 	if input.Username == "" {
@@ -56,4 +89,36 @@ func (s *Service) CreateUser(input CreateUserInput) error {
 	err = s.repo.CreateUser(context.TODO(), user)
 	return err
 
+}
+
+func (s *Service) Login(input LoginInput) (string, error) {
+	// check if user exists
+	//
+	user := &User{
+		Username: input.Username,
+	}
+
+	if input.Username == "" {
+		return "", errors.New("Insert email")
+	}
+
+	if input.Password == "" {
+		return "", errors.New("Insert password")
+	}
+
+	fetchedUser, err := s.repo.FetchUserByUsername(user)
+	if err != nil {
+		return "", errors.New("Fetch failed")
+	}
+
+	if err = bcrypt.CompareHashAndPassword([]byte(fetchedUser.Password), []byte(input.Password)); err != nil {
+		return "", errors.New("Invalid credentials")
+	}
+
+	jwt, err := generateToken(user.Username)
+	if err != nil {
+		return "", errors.New("Failed to generate JWT token")
+	}
+
+	return jwt, nil
 }
