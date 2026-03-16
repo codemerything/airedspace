70f89beac1c0191b6ffd0cdb393a7d300eca598f
Emery <mremmanueledem@gmail.com>
2026-03-16 05:53:33 +0100

    build: add more structs for database schema

--- models.go
+++ models.go
@@ -12,3 +12,25 @@ type User struct {
 	CreatedAt time.Time `json:"created_at" db:"created_at"`
 	UpdatedAt time.Time `json:"updated_at" db:"updated_at"`
 }
+
+type Films struct {
+	FilmID           int64  `json:"id" db:"id"`
+	Title            string `json:"title" db:"title"`
+	TMDB_ID          string `json:"tmdb_id" db:"tmdb_id"`
+	Year             string `json:"year" db:"year"`
+	Poster           string `json:"image" db:"image"`
+	ForegroundPoster string `json:"foreground_poster" db:"foreground_poster"`
+	Description      string `json:"desc" db:"desc"`
+	TagLine          string `json:"tag_line" db:"tag_line"`
+	Time             string `json:"movie_time" db:"movie_time"`
+}
+
+type Review struct {
+	ID        int64   `json:"id" db:"id"`
+	FilmID    int64   `json:"film_id" db:"film_id"`
+	UserID    int64   `json:"user_id" db:"user_id"`
+	AudioURL  string  `json:"audio_url" db:"audio_url"`
+	Stars     float64 `json:"stars" db:"stars"`
+	TMDB_ID   string  `json:"tmdb_id" db:"tmdb_id"`
+	IsRewatch int64   `json:"is_rewatch" db:"is_rewatch"`
+}
