6097ab8ff34145c1e4b7a4a080db29ed7da35934
Emery <mremmanueledem@gmail.com>
2026-03-17 03:41:52 +0100

    feat: fetchfilmdetails function basically uses the movie id to fetch for the movie details from tmdb and sends it to the user

--- tmdb.go
+++ tmdb.go
@@ -14,6 +14,18 @@ type TMDB struct {
 	baseURL string
 }
 
+type TMDBFilmDetails struct {
+	FilmID       int    `json:"id"`
+	Title        string `json:"original_title"`
+	TagLine      string `json:"tagline"`
+	Description  string `json:"overview"`
+	BackdropPath string `json:"backdrop_path"`
+	PosterPath   string `json:"poster_path"`
+	Year         string `json:"release_date"`
+	Time         int    `json:"runtime"`
+	Language     string `json:"original_language"`
+}
+
 type TMDBResult struct {
 	ID int `json:"id"`
 }
@@ -36,13 +48,15 @@ func NewTMDB(apiKey, baseURL string) *TMDB {
 
 func (t *TMDB) FetchMovieID(search string) (int, error) {
 	req, err := http.NewRequest("GET", t.baseURL+"search/movie?query="+search, nil)
+	fmt.Print(req)
 	if err != nil {
 		return 0, fmt.Errorf("Failed request: %w", err)
 	}
 
-	req.Header.Set("Authorization", "Bearer"+t.apiKey)
+	req.Header.Set("Authorization", "Bearer "+t.apiKey)
 
 	resp, err := t.client.Do(req)
+	fmt.Println(resp.StatusCode)
 	if err != nil {
 		return 0, fmt.Errorf("search failed: %w", err)
 	}
@@ -62,7 +76,7 @@ func (t *TMDB) FetchMovieID(search string) (int, error) {
 	return data.Results[0].ID, nil
 }
 
-func (t *TMDB) FetchDetails(id int) (Films, error) {
+func (t *TMDB) FetchFilmDetails(id int) (Films, error) {
 	// start with the request
 	req, err := http.NewRequest("GET", t.baseURL+"movie/"+strconv.Itoa(id), nil)
 
@@ -70,9 +84,29 @@ func (t *TMDB) FetchDetails(id int) (Films, error) {
 		return Films{}, errors.New("Request failed")
 	}
 
-	req.Header.Set("Authorization", "Bearer"+t.apiKey)
+	req.Header.Set("Authorization", "Bearer "+t.apiKey)
 	resp, err := t.client.Do(req)
 	if err != nil {
 		return Films{}, fmt.Errorf("search failed: %w", err)
 	}
+
+	defer resp.Body.Close()
+	var data TMDBFilmDetails
+
+	err = json.NewDecoder(resp.Body).Decode(&data)
+	if err != nil {
+		return Films{}, fmt.Errorf("failed to decode response: %w", err)
+	}
+
+	return Films{
+		Title:          data.Title,
+		TMDB_ID:        data.FilmID,
+		Year:           data.Year,
+		Poster:         data.PosterPath,
+		BackdropPoster: data.BackdropPath,
+		Description:    data.Description,
+		TagLine:        data.TagLine,
+		Time:           data.Time,
+	}, nil
+
 }
